19a9177c450c508c096ddb047c80c130297f86bf
realEmadAbbasnezhad <real_emad_abbasnezhad@proton.me>
2026-02-03 16:47:13 +0330

    feat: add swagger

--- apps/gateway-rest/src/main.ts
+++ apps/gateway-rest/src/main.ts
@@ -2,10 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { GatewayRestModule } from './gateway-rest.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayRestModule);
   const configService = app.get(ConfigService);
+
+  const swaggerEnabled = configService.get<boolean>(
+    'SWAGGER_ENABLED',
+  ) as boolean;
+  if (swaggerEnabled) {
+    const swaggerPath = configService.get<string>('SWAGGER_PATH') as string;
+    const documentBuilder = new DocumentBuilder()
+      .setTitle('RESTful Gateway API')
+      .addBearerAuth(undefined, 'jwt')
+      .build();
+    const document = SwaggerModule.createDocument(app, documentBuilder);
+    SwaggerModule.setup('swagger', app, document);
+    Logger.log(
+      `Swagger is started and listening on "/${swaggerPath}"!`,
+      'Bootstrap',
+    );
+  }
 
   const port = configService.get<number>('PORT_GATEWAY_REST') as number;
   Logger.log(`RESTful Gateway is running on port ${port}`, 'Bootstrap');
--- libs/common/src/config/env.schema.ts
+++ libs/common/src/config/env.schema.ts
@@ -3,4 +3,6 @@ import * as Joi from 'joi';
 export const ENV_SCHEMA = Joi.object({
   PORT_GATEWAY_REST: Joi.number().port().default(3000),
   PORT_AUTH: Joi.number().port().default(3001),
+  SWAGGER_ENABLED: Joi.boolean().default(true),
+  SWAGGER_PATH: Joi.string().default('swagger'),
 } as Joi.SchemaMap);
