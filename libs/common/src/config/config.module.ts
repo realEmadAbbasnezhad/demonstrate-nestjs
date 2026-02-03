4f6c437c4d13d2067d999a2167b47d0642f7f077
realEmadAbbasnezhad <real_emad_abbasnezhad@proton.me>
2026-02-03 15:48:58 +0330

    feat: add common lib, add config module

---
+++ libs/common/src/config/config.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import {
+  ConfigService,
+  ConfigModule as NestConfigModule, // due to conflict with our "ConfigModule"
+} from '@nestjs/config';
+import { ENV_SCHEMA } from './env.schema';
+
+@Module({
+  imports: [NestConfigModule.forRoot({ validationSchema: ENV_SCHEMA })],
+  providers: [ConfigService],
+  exports: [ConfigService],
+})
+export class ConfigModule {}
---
+++ libs/common/src/config/env.schema.ts
@@ -0,0 +1,5 @@
+import * as Joi from 'joi';
+
+export const ENV_SCHEMA = Joi.object({
+  PORT_GATEWAY_REST: Joi.number().port().default(3000),
+} as Joi.SchemaMap);
